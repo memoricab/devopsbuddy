ee211bbe928138d89bdfb8e77aa134e7519e0841
celenmeh <celenmeh@gmail.com>
2018-01-30 00:46:20 +0300

    h2 console integration and jpa repo integration tests

--- src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
+++ src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
@@ -2,6 +2,9 @@ package com.devopsbuddy.config;
 
 import com.devopsbuddy.backend.service.EmailService;
 import com.devopsbuddy.backend.service.MockEmailService;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,7 +16,14 @@ import org.springframework.context.annotation.PropertySource;
 public class DevelopmentConfig {
 
     @Bean
-    public EmailService emailService(){
+    public EmailService emailService() {
         return new MockEmailService();
+    }
+
+    @Bean
+    public ServletRegistrationBean h2ConsoleServletRegistration() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
+        bean.addUrlMappings("/console/*");
+        return bean;
     }
 }
